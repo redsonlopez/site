dbbef3b25cc981b07c1cf8b8e596eef079bd731d
redsonlopez <redsonlopez@gmail.com>
2026-04-02 21:59:44 -0300

    change file case

--- components/site-header.js
+++ components/site-header.js
@@ -9,10 +9,8 @@ class SiteHeader extends HTMLElement {
           <ul class="simple-ul">
             <li><a href="./" title="Click para visualizar meu portfólio">Portfolio</a></li>
             <li><a href="./stacks.html" title="Click para visualizar minhas stacks">Stacks</a></li>
-            <!--<li><a href="./blog.html" title="Click para visualizar meu blog">Blog</a></li>-->
           </ul>
         </nav>
-        <!--<site-navbar></site-navbar>-->
       </header>
     `;
   }
